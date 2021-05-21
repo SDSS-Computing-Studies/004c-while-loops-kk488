8d6fe90362de02ac1b91cee28df952416decc9d2
kk488 <78923924+kk488@users.noreply.github.com>
2021-05-20 18:16:59 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -15,3 +15,16 @@ outputs:
 Access granted
 Access denied
 """
+print("enter correct username and password")
+inputName=""
+inputPW=""
+while inputName!="admin" and inputPW!="12345":
+    inputName=input("enter username:")
+    inputPW=input("enter password:")
+    if inputName=="admin" and inputPW=="12345":
+        print("Access granted.")
+        break
+    else:
+        print("Access denied.")
+        inputName=input("enter username:")
+        inputPW=input("enter password:")
